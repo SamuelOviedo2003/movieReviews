46e911ba76d06a91cfbb505c62f3642c461c4fa9
SamuelOviedo2003 <92952464+SamuelOviedo2003@users.noreply.github.com>
2023-02-09 11:31:31 -0500

    about

--- movie/views.py
+++ movie/views.py
@@ -5,5 +5,9 @@ def home(request):
     #return render(request,'home.html')
     return render(request,'home.html',{'name':'Samuel Oviedo'})
 
+def about(request):
+    #return render(request,'home.html')
+    return HttpResponse('<h1>Welcome to About page</h1>')
+
 
 # Create your views here.
